38ec038845c583470c74fdcb7e84271ef1f6d1e3
sunil kumar <sunilkumar36912@gmail.com>
2023-03-03 15:14:11 +0530

    first node commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+var colors = require('colors');
+ 
+console.log('hello'.green); // outputs green text
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end("<h1 style='color:pink;'>Garvi </h1>");
+}).listen("5000");
+console.log()
