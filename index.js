40c5027a292665d45525a6f6c46b5fef5b75d783
sunil kumar <sunilkumar36912@gmail.com>
2023-03-11 11:51:58 +0530

    file system commit

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
-const http = require('http');
-var data = require('./data');
-var colors = require('colors');
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
-// const data={
-//     'name':'sunil',
-//     'class':'12',
-//     'designation':'developer'
-// }
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   var q = url.parse(req.url, true).query;
+//   var txt = q.year + " " + q.month;
+//   res.end(txt);
+// }).listen(8080);
 
-console.log(process.argv);
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     var filename= process.argv[2];
+//     var content=process.argv[3];
+//    fs.writeFileSync(filename,content);
+//     res.end(txt);
+//   }).listen(8080);
+  
+
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+
+
+//   fs.readFile('reafile.html', (err, data)=> {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write(data);
+//     return res.end();
+//   });
  
-console.log('hello'.green); // outputs green text
-http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(JSON.stringify(data));
-    res.end();
-}).listen("5000");
+  fs.appendFile("myappend","<h1>My Append Content</h1>",(err)=>{
+if (err) throw err;
+console.log('saved');
+
+});
+  }).listen(8080);
 
