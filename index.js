c2f82ff96b18e15e467eebf31e666d16baccfc8f
sunil kumar <sunilkumar36912@gmail.com>
2023-03-11 12:55:49 +0530

    html pages added

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 const express= require('express');
+const path=require('path');
 const app=express();
-app.get('',(req,res)=>{
-res.send('<h1>This is my Home Page</h1>');
-});
-app.get('/about',(req,res)=>{
-  res.send('<h1>This is my About Page</h1>');
-  });
-  app.get('/help',(req,res)=>{
-    console.log(req.query.name);
-    res.send(`<h1>This is my Help Page</h1>`+req.query.name);
-    });
 
-    app.get('/contact',(req,res)=>{
-      //res.send('<h1>This is my Contact Page</h1>');
-     
-      res.send(`
-      <input type="text" placeholder="Enter Name"
-   /><button type="button"  >Click Me</button>
-   `);
-      });
+const publicpath=path.join(__dirname,'public');
+//console.log(publicpath);
+//app.use(express.static(publicpath));
+app.get('',(req,resp)=>{
+resp.sendFile(`${publicpath}/about.html`)
+})
+app.get('*',(req,resp)=>{
+  resp.sendFile(`${publicpath}/404.html`)
+  })
 
     app.listen(5000);
